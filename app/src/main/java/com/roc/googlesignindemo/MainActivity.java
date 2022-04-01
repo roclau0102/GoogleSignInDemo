6dc37aba82a5d242221d38bfac5eb5c5efda0042
roc <roclau0102@gmail.com>
2022-04-01 23:30:48 +0800

    supoort google sign in v2

--- app/src/main/java/com/roc/googlesignindemo/MainActivity.java
+++ app/src/main/java/com/roc/googlesignindemo/MainActivity.java
@@ -10,12 +10,15 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.google.android.gms.auth.api.identity.BeginSignInRequest;
 import com.google.android.gms.auth.api.identity.BeginSignInResult;
+import com.google.android.gms.auth.api.identity.GetSignInIntentRequest;
 import com.google.android.gms.auth.api.identity.Identity;
 import com.google.android.gms.auth.api.identity.SignInClient;
 import com.google.android.gms.auth.api.identity.SignInCredential;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.common.api.CommonStatusCodes;
 import com.google.android.gms.tasks.OnFailureListener;
@@ -23,24 +26,33 @@ import com.google.android.gms.tasks.OnSuccessListener;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    // for one-tap sign in
     private SignInClient oneTapClient;
     private BeginSignInRequest signInRequest;
-
     private static final int REQ_ONE_TAP = 2;  // Can be any integer unique to the Activity.
-    private boolean showOneTapUI = true;
+
+    // for google sign in
+    private GoogleSignInClient mGoogleSignInClient;
+    private static final int REQUEST_CODE_GOOGLE_SIGN_IN = 1; /* unique request id */
 
     private static final String TAG = "FUCK";
+    private String webClientId = "1098155987904-qacv95n47rvt453v3snvhs92g2bpfst5.apps.googleusercontent.com";   // use web client id, not android client id
+    private TextView text;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button button = findViewById(R.id.button);
+        text = findViewById(R.id.text);
+        button = findViewById(R.id.button);
         button.setOnClickListener(this);
 
-        String androidClientId = "1098155987904-ebg14scpmhtkihbeamajdm5ik88kp73j.apps.googleusercontent.com";
-        String webClientId = "1098155987904-qacv95n47rvt453v3snvhs92g2bpfst5.apps.googleusercontent.com";   // use this, not android client id
+        initOneTapSignIn();
+    }
+
+    void initOneTapSignIn() {
         oneTapClient = Identity.getSignInClient(this);
         signInRequest = BeginSignInRequest.builder()
 //                .setPasswordRequestOptions(BeginSignInRequest.PasswordRequestOptions.builder()
@@ -58,8 +70,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 .build();
     }
 
-    @Override
-    public void onClick(View view) {
+    private void requestOneTapSignIn() {
         oneTapClient.beginSignIn(signInRequest)
                 .addOnSuccessListener(this, new OnSuccessListener<BeginSignInResult>() {
                     @Override
@@ -83,6 +94,40 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 });
     }
 
+    private void requestGoogleSignIn() {
+        GetSignInIntentRequest request =
+                GetSignInIntentRequest.builder()
+                        .setServerClientId(webClientId)
+                        .build();
+
+        Identity.getSignInClient(this)
+                .getSignInIntent(request)
+                .addOnSuccessListener(
+                        result -> {
+                            try {
+                                startIntentSenderForResult(
+                                        result.getIntentSender(),
+                                        REQUEST_CODE_GOOGLE_SIGN_IN,
+                                        /* fillInIntent= */ null,
+                                        /* flagsMask= */ 0,
+                                        /* flagsValue= */ 0,
+                                        /* extraFlags= */ 0,
+                                        /* options= */ null);
+                            } catch (IntentSender.SendIntentException e) {
+                                Log.e(TAG, "Google Sign-in failed");
+                            }
+                        })
+                .addOnFailureListener(
+                        e -> {
+                            Log.e(TAG, "Google Sign-in failed", e);
+                        });
+    }
+
+    @Override
+    public void onClick(View view) {
+        requestGoogleSignIn();
+    }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
@@ -97,6 +142,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     if (idToken !=  null) {
                         // Got an ID token from Google. Use it to authenticate
                         // with your backend.
+                        text.setText(idToken);
                         Log.d(TAG, "Got ID token: " + idToken);
                     }
                     if (username != null) {
@@ -118,9 +164,23 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                             Log.d(TAG, "one-tap encountered a network error, try again");
                             break;
                         default:
-                            Log.d(TAG, "oneTapClient.getSignInCredentialFromIntent failed, " + e.getLocalizedMessage());
+                            Log.d(TAG, "not google accounts, " + e.getLocalizedMessage());
                             break;
                     }
+                }
+                break;
+            case REQUEST_CODE_GOOGLE_SIGN_IN:
+                try {
+                    SignInCredential credential = Identity.getSignInClient(this).getSignInCredentialFromIntent(data);
+                    // Signed in successfully - show authenticated UI
+                    String idToken = credential.getGoogleIdToken();
+                    if (idToken != null) {
+                        text.setText(idToken);
+                        Log.d(TAG, "Got ID token: " + idToken);
+                    }
+                } catch (ApiException e) {
+                    // The ApiException status code indicates the detailed failure reason.
+                    Log.d(TAG, "getSignInCredentialFromIntent failed, " + e.getLocalizedMessage());
                 }
                 break;
         }
